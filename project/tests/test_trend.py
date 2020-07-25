3dacd476c6f6f1ad08091eeb2c642eec56e55631
srilakshmee <srilakshmi.krishnamoorthy@gmail.com>
2020-07-25 12:54:33 +0530

    testing workflow

--- project/tests/test_trend.py
+++ project/tests/test_trend.py
@@ -21,5 +21,17 @@ class TestTrendBlueprint(BaseTestCase):
             self.assertTrue(len(data["artists"]) == 10)
             self.assertEqual(response.status_code, 201)
 
+    def test_genre(self):
+        """ Test for trend API"""
+        with self.client:
+            response = self.client.get(
+                '/genre',
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            #self.assertTrue(data["artists"][0]["artists"] == 'The Weeknd')
+            #self.assertTrue(data["avg_duration"] == '186.4537 sec')
+            #self.assertTrue(len(data["artists"]) == 10)
+            self.assertEqual(response.status_code, 201)
 if __name__ == '__main__':
     unittest.main()
